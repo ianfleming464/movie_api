08e6ff0fbb6f256dd29ff218a82a789c651df1d6
ianfleming464 <60020259+ianfleming464@users.noreply.github.com>
2020-04-02 23:05:33 +0200

    Update index.js

--- index.js
+++ index.js
@@ -132,71 +132,71 @@ let movies = [
   }
 ];
 
-let directors = [
-  {
-    name: "John Hughes",
-    born: "February 18, 1950"
-  },
-  {
-    name: "Jim Abrahams",
-    born: "May 10, 1944"
-  },
-  {
-    name: "John McTiernan",
-    born: "January 8, 1951"
-  },
-  {
-    name: "Ridley Scott",
-    born: "November 30, 1937"
-  },
-  {
-    name: "James Cameron",
-    born: "August 16, 1954"
-  },
-  {
-    name: "Rob Reiner",
-    born: "March 6, 1947"
-  },
-  {
-    name: "Robert Zemeckis",
-    born: "May 14, 1951"
-  },
-  {
-    name: "Martin Brest",
-    born: "August 8, 1951"
-  },
-  {
-    name: "Albert Magnoli",
-    born: "January 31, 1954"
-  },
-  {
-    name: "Ivan Reitman",
-    born: "October 27, 1946"
-  }
-];
+// let directors = [
+//   {
+//     name: "John Hughes",
+//     born: "February 18, 1950"
+//   },
+//   {
+//     name: "Jim Abrahams",
+//     born: "May 10, 1944"
+//   },
+//   {
+//     name: "John McTiernan",
+//     born: "January 8, 1951"
+//   },
+//   {
+//     name: "Ridley Scott",
+//     born: "November 30, 1937"
+//   },
+//   {
+//     name: "James Cameron",
+//     born: "August 16, 1954"
+//   },
+//   {
+//     name: "Rob Reiner",
+//     born: "March 6, 1947"
+//   },
+//   {
+//     name: "Robert Zemeckis",
+//     born: "May 14, 1951"
+//   },
+//   {
+//     name: "Martin Brest",
+//     born: "August 8, 1951"
+//   },
+//   {
+//     name: "Albert Magnoli",
+//     born: "January 31, 1954"
+//   },
+//   {
+//     name: "Ivan Reitman",
+//     born: "October 27, 1946"
+//   }
+// ];
 
-let genres = [
-  {
-    name: "Comedy",
-    description:
-      "A comedy film is a genre of film in which the main emphasis is on humor. These films are designed to make the audience laugh through amusement and most often work by exaggerating characteristics for humorous effect. Films in this style traditionally have a happy ending (black comedy being an exception)."
-  },
-  {
-    name: "Science fiction",
-    description:
-      "Science fiction (or sci-fi) is a genre that uses speculative, fictional science-based depictions of phenomena that are not fully accepted by mainstream science, such as extraterrestrial lifeforms, alien worlds, extrasensory perception and time travel, along with futuristic elements. Science fiction films have often been used to focus on political or social issues, and to explore philosophical issues like the human condition."
-  },
-  {
-    name: "Adventure",
-    description:
-      "Adventure films are a genre of film that typically use their action scenes to display and explore exotic locations in an energetic way. Main plot elements include quests for lost continents and exotic setting; struggles and situations that confront the main characters, the creation of empires, characters embarking on treasure and heroic journeys, travels, explorations, quests and searches for the unknown usually also having to overcome an adversary."
-  },
-  {
-    name: "Drama",
-    description:
-      "Drama is a genre of narrative fiction intended to be more serious than humorous in tone. Drama of this kind is usually qualified with additional terms that specify its particular subgenre. These terms tend to indicate a particular setting or subject-matter, or else they qualify the otherwise serious tone of a drama with elements that encourage a broader range of moods."
-  }
-];
+// let genres = [
+//   {
+//     name: "Comedy",
+//     description:
+//       "A comedy film is a genre of film in which the main emphasis is on humor. These films are designed to make the audience laugh through amusement and most often work by exaggerating characteristics for humorous effect. Films in this style traditionally have a happy ending (black comedy being an exception)."
+//   },
+//   {
+//     name: "Science fiction",
+//     description:
+//       "Science fiction (or sci-fi) is a genre that uses speculative, fictional science-based depictions of phenomena that are not fully accepted by mainstream science, such as extraterrestrial lifeforms, alien worlds, extrasensory perception and time travel, along with futuristic elements. Science fiction films have often been used to focus on political or social issues, and to explore philosophical issues like the human condition."
+//   },
+//   {
+//     name: "Adventure",
+//     description:
+//       "Adventure films are a genre of film that typically use their action scenes to display and explore exotic locations in an energetic way. Main plot elements include quests for lost continents and exotic setting; struggles and situations that confront the main characters, the creation of empires, characters embarking on treasure and heroic journeys, travels, explorations, quests and searches for the unknown usually also having to overcome an adversary."
+//   },
+//   {
+//     name: "Drama",
+//     description:
+//       "Drama is a genre of narrative fiction intended to be more serious than humorous in tone. Drama of this kind is usually qualified with additional terms that specify its particular subgenre. These terms tend to indicate a particular setting or subject-matter, or else they qualify the otherwise serious tone of a drama with elements that encourage a broader range of moods."
+//   }
+// ];
 
 // MIDDLEWARE
 
@@ -211,13 +211,6 @@ app.use(morgan("common"));
 // Accesses the public directory -
 
 app.use(express.static("public"));
-
-// Error handling
-
-app.use(function(err, req, res, next) {
-  console.error(err.stack);
-  res.status(500).send("There has been an error.");
-});
 
 // Endpoints
 
@@ -235,17 +228,17 @@ app.get("/movies", (req, res) => {
 
 // Returns data about single movie by title
 
-// app.get("/movies/:title", (req, res) => {
-//   res.json(
-//     movies.find(movie => {
-//       return movie.title === req.params.title;
-//     })
-//   );
-// });
-
 app.get("/movies/:title", (req, res) => {
-  res.send("Successful GET request for obtaining movie by title!");
+  res.json(
+    movies.find(movie => {
+      return movie.title === req.params.title;
+    })
+  );
 });
+
+// app.get("/movies/:title", (req, res) => {
+//   res.send("Successful GET request for obtaining movie by title!");
+// });
 
 // Return data about a genre by name
 
@@ -296,6 +289,13 @@ app.delete("/users/:username/favourites", (req, res) => {
 
 app.delete("/users/:username", (req, res) => {
   res.send("Succesful DELETE of existing user");
+});
+
+// Error handling
+
+app.use(function(err, req, res, next) {
+  console.error(err.stack);
+  res.status(500).send("There has been an error.");
 });
 
 // Listen for requests
