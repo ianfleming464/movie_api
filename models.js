e655b5c85f8fc4a51abcd0ef22af11208af3b6f0
ianfleming464 <60020259+ianfleming464@users.noreply.github.com>
2020-04-14 13:30:28 +0200

    Create models.js

---
+++ models.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+
+var movieSchema = mongoose.Schema({
+  Title: { type: String, required: true },
+  Description: { type: String, required: true },
+  Genre: {
+    Name: String,
+    Description: String
+  },
+  Director: {
+    Name: String,
+    Born: String,
+    Died: String,
+    Biography: String
+  },
+  // Note - delete Actors for finished app?
+  Actors: [String],
+  ImagePath: String,
+  Year: String,
+  Featured: Boolean
+});
+
+var userSchema = mongoose.Schema({
+  Username: { type: String, required: true },
+  Password: { type: String, required: true },
+  Email: { type: String, required: true },
+  Birthday: Date,
+  FavouriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: "Movie" }]
+});
+
+var Movie = mongoose.model("Movie", movieSchema);
+var User = mongoose.model("User", userSchema);
+
+module.exports.Movie = Movie;
+module.exports.User = User;
