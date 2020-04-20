fccf89c319ecbabcce90e2bfff44c64408507067
ianfleming464 <60020259+ianfleming464@users.noreply.github.com>
2020-04-20 22:42:13 +0200

    Update models.js
    Added validation logic to user schema

--- models.js
+++ models.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
 var movieSchema = mongoose.Schema({
   Title: { type: String, required: true },
@@ -20,13 +21,21 @@ var movieSchema = mongoose.Schema({
   Featured: Boolean
 });
 
-var userSchema = mongoose.Schema({
+let userSchema = mongoose.Schema({
   Username: { type: String, required: true },
   Password: { type: String, required: true },
   Email: { type: String, required: true },
   Birthday: Date,
   FavouriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: "Movie" }]
 });
+
+userSchema.statics.hashPassword = password => {
+  return bcrypt.hashSync(password, 10);
+};
+
+userSchema.methods.validatePassword = password => {
+  return bcrypt.compareSync(password, this.Password);
+};
 
 var Movie = mongoose.model("Movie", movieSchema);
 var User = mongoose.model("User", userSchema);
