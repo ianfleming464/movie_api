5cdcebe1c0f87b44f0b7cfa55cbc94cfa85e6d31
ianfleming464 <60020259+ianfleming464@users.noreply.github.com>
2020-04-30 23:47:57 +0200

    Update main-view.jsx

--- client/src/components/main-view/main-view.jsx
+++ client/src/components/main-view/main-view.jsx
@@ -1,17 +1,36 @@
 import React from "react";
 import axios from "axios";
 
-class MainView extends React.Component {
-  constructor() {
-    // Call the superclass constructor so React can initialize it
-    super();
-
-    // Initialize the state to an empty object so we can destructure it later
-    this.state = {};
+export class MainView extends React.Component {
+  // One of the hooks available in a React component
+  componentDidMount() {
+    axios
+      .get("https://my1980smoviesapi.herokuapp.com/movies")
+      .then(response => {
+        // Assign the result to the state
+        this.setState({
+          movies: response.data
+        });
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
   }
-  //This overrides the render() method of the super class. No need
-  //to call super() though, as it does nothing by default.
+
   render() {
-    return <div className="main-view"></div>;
+    // If the state isn't initialzed, this will throw on runtime
+    const { movies } = this.state;
+    //Before the movies have been loaded
+    if (!movies) return <div className="main-view" />;
+
+    return (
+      <div className="main-view">
+        {movies.map(movie => (
+          <div className="movie-card" key={movie._id}>
+            {movie.Title}
+          </div>
+        ))}
+      </div>
+    );
   }
 }
