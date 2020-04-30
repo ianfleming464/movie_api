278c90e50a84bd96617720948611fb815e9c8321
ianfleming464 <60020259+ianfleming464@users.noreply.github.com>
2020-05-01 00:02:36 +0200

    Update index.js

--- index.js
+++ index.js
@@ -25,20 +25,20 @@ mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnified
 
 app.use(cors());
 
-let allowedOrigins = ["http://localhost:1234', 'http://testsite.com"];
+// let allowedOrigins = ["http://localhost:1234', 'http://testsite.com"];
 
-app.use(
-  cors({
-    origin: (origin, callback) => {
-      if (!origin) return callback(null, true);
-      if (allowedOrigins.indexOf(origin) === -1) {
-        let message = "The CORS policy for this application doesn't allow access from origin " + origin;
-        return callback(new Error(message), false);
-      }
-      return callback(null, true);
-    }
-  })
-);
+// app.use(
+//   cors({
+//     origin: (origin, callback) => {
+//       if (!origin) return callback(null, true);
+//       if (allowedOrigins.indexOf(origin) === -1) {
+//         let message = "The CORS policy for this application doesn't allow access from origin " + origin;
+//         return callback(new Error(message), false);
+//       }
+//       return callback(null, true);
+//     }
+//   })
+// );
 
 // adding bodyParser -
 
