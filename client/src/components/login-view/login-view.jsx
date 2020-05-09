30ab1206b432df56d97f01dbd2eef8946cb49ed1
ianfleming464 <60020259+ianfleming464@users.noreply.github.com>
2020-05-10 00:06:01 +0200

    Updated Login view and styling

--- client/src/components/login-view/login-view.jsx
+++ client/src/components/login-view/login-view.jsx
@@ -1,4 +1,9 @@
 import React, { useState } from "react";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import "./login-view.scss";
+import axios from "axios";
 
 export function LoginView(props) {
   const [username, setUsername] = useState(""); // Array destructuring - first item is current value, second is a function that lets us update it
@@ -12,18 +17,25 @@ export function LoginView(props) {
   };
 
   return (
-    <form>
-      <label>
-        Username:
-        <input type="text" value={username} onChange={e => setUsername(e.target.value)} />
-      </label>
-      <label>
-        Password:
-        <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
-      </label>
-      <button type="button" onClick={handleSubmit}>
-        Submit
-      </button>
-    </form>
+    <Container className="login-container">
+      <Form>
+        <Form.Group controlId="formBasicUsername">
+          <Form.Label>Username</Form.Label>
+          <Form.Control type="text" value={username} onChange={e => setUsername(e.target.value)} />
+        </Form.Group>
+        <Form.Group controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" value={password} onChange={e => setPassword(e.target.value)} />
+        </Form.Group>
+        <p className="register-link">
+          Not registered? <a href="#">Sign up!</a>
+        </p>
+        <Button className="button-login" variant="primary" type="submit" onClick={handleSubmit}>
+          Submit
+        </Button>
+      </Form>
+    </Container>
   );
 }
+
+// Above has a link to the registration page, once I figure out how to do it..
